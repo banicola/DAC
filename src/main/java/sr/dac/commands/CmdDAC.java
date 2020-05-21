a8dc182dc7b9ad79fa5032af8477ee1f153c8002
banicola <bastien.nicolas@student.unamur.be>
2020-05-21 20:32:49 +0200

    Add first menu

--- src/main/java/sr/dac/commands/CmdDAC.java
+++ src/main/java/sr/dac/commands/CmdDAC.java
@@ -1,6 +1,7 @@
 package sr.dac.commands;
 
-import org.bukkit.Bukkit;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -10,24 +11,57 @@ import sr.dac.main.Config;
 import sr.dac.main.Lang;
 import sr.dac.main.Main;
 
+import java.awt.*;
+import java.util.Collections;
+
 public class CmdDAC implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-        if(command.getName().equalsIgnoreCase("dac") && args.length > 0 && args[0].contentEquals("reload")){
-            if(sender.hasPermission("dac.reload")){
-                Config.reloadConfig();
-                Lang.reloadConfig();
-                Bukkit.getPluginManager().disablePlugin(Main.getPlugin());
-                Bukkit.getPluginManager().enablePlugin(Main.getPlugin());
-                Main.getPlugin().getLogger().info(ChatColor.translateAlternateColorCodes('&',Main.f.getString("debug.reloadConfig")));
-                if(sender instanceof Player){
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',Main.f.getString("name")+" "+Main.f.getString("debug.reloadConfig")));
+        if(command.getName().equalsIgnoreCase("dac")){
+            if(args.length > 0 && args[0].contentEquals("reload")){
+                if(sender.hasPermission("dac.reload")){
+                    Config.reloadConfig();
+                    Lang.reloadConfig();
+                    Main.getPlugin().getLogger().info(ChatColor.translateAlternateColorCodes('&',Main.f.getString("debug.reloadConfig")));
+                    if(sender instanceof Player){
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',Main.f.getString("name")+" "+Main.f.getString("debug.reloadConfig")));
+                    }
+                }else{
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',Main.f.getString("debug.noPermission")));
                 }
-            }else{
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',Main.f.getString("debug.noPermission")));
+            } else {
+                if(sender instanceof Player){
+                    String topLayer = Main.f.getString("menu.title");
+                    String editedLayer = removeChatColor(topLayer);
+                    int lineNeeded = 45-editedLayer.length();
+                    String newTopLayer = "&8&l"+String.join("", Collections.nCopies((int) Math.round(lineNeeded/2.0), "-"))+" "+topLayer+" "+"&8&l"+String.join("", Collections.nCopies((int) Math.round(lineNeeded/2.0), "-"));
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', newTopLayer));
+
+                    TextComponent join = new TextComponent(ChatColor.translateAlternateColorCodes('&',Main.f.getString("menu.join")));
+                    join.setClickEvent(new ClickEvent( ClickEvent.Action.SUGGEST_COMMAND, "/dac join" ));
+                    TextComponent leave = new TextComponent(ChatColor.translateAlternateColorCodes('&',Main.f.getString("menu.leave")));
+                    leave.setClickEvent(new ClickEvent( ClickEvent.Action.SUGGEST_COMMAND, "/dac leave" ));
+                    TextComponent list = new TextComponent(ChatColor.translateAlternateColorCodes('&',Main.f.getString("menu.list")));
+                    list.setClickEvent(new ClickEvent( ClickEvent.Action.SUGGEST_COMMAND, "/dac list" ));
+                    sender.spigot().sendMessage(join);
+                    sender.spigot().sendMessage(leave);
+                    sender.spigot().sendMessage(list);
+
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8&l"+String.join("", Collections.nCopies(45, "-"))));
+                } else {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',Main.f.getString("name")+" "+Main.f.getString("debug.playerOnly")));
+                }
             }
             return true;
         }
         return false;
+    }
+
+    private String removeChatColor(String topLayer){
+        if(!topLayer.contains("&")){
+            return topLayer;
+        } else {
+            return removeChatColor(topLayer.substring(0, topLayer.indexOf("&"))+topLayer.substring(topLayer.indexOf("&") + 2));
+        }
     }
 }
